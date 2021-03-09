8216f2e274cbc5c54a9dc1abb0ca960112884bae
karol <karol.malysa@interia.pl>
2021-03-09 19:55:16 +0100

    założenie: mam dużo konstruktorów i nie jestem pewny co do kolejności pól, trocję się w tym po prostu gubię.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import House.House;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        House house1 = new House("wall1", "floor1", "room1", "window1"."door1","garage");
+//        House house2 = new House();  // - założenie: mamy dużo konstruktorów i nie jesteśmy pewni co do kolejności pól
+//                                                     i troche zaczynamy się w tym gubuć
+
+
 
     }
 }
