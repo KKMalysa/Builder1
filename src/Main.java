10343167a4eb8bc04381cc434d11e1574e8d4df5
karol <karol.malysa@interia.pl>
2021-03-09 22:41:03 +0100

    implementacja wzorca builder (z klasą wewnętrzną)

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,21 @@ public class Main {
 
     public static void main(String[] args) {
 
-        House house1 = new House("wall1", "floor1", "room1", "window1","door1","garage");
+//        House house1 = new House("wall1", "floor1", "room1", "window1","door1","garage");
 //        House house2 = new House();  // - założenie: mam dużo konstruktorów i nie jestem pewny co do kolejności pól
 //                                                     i troche zaczynam się w tym gubić
 
 
+        House house3 = new House.HouseBuilder()
+                .buildDoors("jakieś drzwi")
+                .buildGarage("garaż")
+                .buildWalls("sciany jako trzecie")
+                .buildFloors("floors")
+                .build(); // <-- ta metoda kończy budowanie i zwraca obiekt.
 
+
+        System.out.println(house3);
     }
+
+
 }
